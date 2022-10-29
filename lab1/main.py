45d39d9e33c379faecc9a7feaf9879f46db19e43
Anastasia Kravchenko <anastasia.kravchenko@sibers.com>
2022-10-29 16:35:59 +0600

    added braces checker

---
+++ lab1/main.py
@@ -0,0 +1,24 @@
+
+def braces():
+    sequence = input("Enter braces sequence:")
+    allowed = "()"
+    if not all(ch in allowed for ch in sequence):
+        print("String contains not allowed characters.")
+        return
+    pairs = 0
+    for ch in sequence:
+        if ch == "(" : pairs += 1
+        if ch == ")": pairs -= 1
+
+        if pairs < 0:
+            print("Sequence is invalid.")
+            return
+
+    if pairs == 0: print("Sequence is valid.")
+    else: print("Sequence is invalid.")
+
+
+
+
+if __name__ == '__main__':
+    braces()
